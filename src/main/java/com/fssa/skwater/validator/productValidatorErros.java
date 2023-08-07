1f026bef4e938e6188963ab676625b9c61d102b2
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-08-07 18:40:42 +0530

    path update

--- src/main/java/com/fssa/skwater/validator/productValidatorErros.java
+++ src/main/java/com/fssa/skwater/validator/productValidatorErros.java
@@ -6,13 +6,14 @@ public interface productValidatorErros {
 	String INVALID_PRODUCTNAME_NULL = "Product name cannot be null.";
 	String INVALID_PRODUCT_DESCRIPTION_NULL = "Product description cannot be null.";
 	String INVALID_PRODUCT_IMAGE_URL_NULL = "product image url can't be null";	
-	String INVALID_PRODUCTNAME = "Invalid product name";
+	String INVALID_PRODUCTNAME1 = "Invalid product name";
 	String INVALID_PRODUCT_PRICE = "Invalid product price.";
-	String INVALID_PRODUCT_IMAGE_URL = "Invalid product image url.";
-	String INVALID_PRODUCT_CAPACITY =  "Product capacity cannot be null.";
+	String INVALID_PRODUCT_IMAGE_URL = "Invalid product image url";
+	String INVALID_PRODUCTNAME =  "Invalid product name";
 	String INVALID_PRODUCT_DESCRIPTION = "Invalid product description";	
 	String PRODUCT_PRICE_FAIL_MSG = "Invalid productprice fail message";
 	String PRODUCT_IMAGE_URL_FAIL_MSG = "Invalid product image url fail message";
 	Object INVALID_PRODUCT_NAME = "Invalid product name";
+	String INVALID_PRODUCT_CAPACITY = "valid product capacity";
 }
 
